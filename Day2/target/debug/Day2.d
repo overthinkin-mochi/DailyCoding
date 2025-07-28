01b8cfa39bb9fb065c67603503d7ca244f6a0da0
grampa.mochi <sebastian.lorca.j@usach.cl>
2025-07-28 11:20:41 -0400

    Limpiar directorios target

--- Day2/target/debug/Day2.d
+++
@@ -1,1 +0,0 @@
-/home/mochi/Projectos/DailyProgramming/Day2/target/debug/Day2: /home/mochi/Projectos/DailyProgramming/Day2/src/main.rs
--- Day2/target/debug/deps/Day2-74948d3a42fb0627.d
+++
@@ -1,5 +0,0 @@
-/home/mochi/Projectos/DailyProgramming/Day2/target/debug/deps/Day2-74948d3a42fb0627.d: src/main.rs
-
-/home/mochi/Projectos/DailyProgramming/Day2/target/debug/deps/Day2-74948d3a42fb0627: src/main.rs
-
-src/main.rs:
